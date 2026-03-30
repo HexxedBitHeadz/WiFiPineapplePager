542b607d014b7c85dfa37544515e03c31a3fb0ee
Aromak <114350234+HexxedBitHeadz@users.noreply.github.com>
2026-03-30 09:38:15 -0400

    Add files via upload

---
+++ reconnaissance/channel_reset/payload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Title:       Channel Reset
+# Author:      Hexxed BitHeadz
+# Description: Cancels any active channel lock and resumes normal channel hopping.
+# Version:     1.0
+
+PINEAPPLE_EXAMINE_RESET
+ALERT "Channel lock cleared"
+VIBRATE "alert"
